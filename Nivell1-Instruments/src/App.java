b3374c4398444e5b2add2ad853b3b4a68df9f85d
Pia <piamunozt@gmail.com>
2024-09-30 13:36:15 +0200

    Create classes and demonstrate loaded just once on terminal

--- Nivell1-Instruments/src/App.java
+++ Nivell1-Instruments/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        MusicalInstrument guitarra = new StringInstrument("guitarra", 250.0);
+        MusicalInstrument bateria = new PercussionInstrument("batería", 250.0);
+        MusicalInstrument flauta = new WindInstrument("flauta", 340.0);
+        MusicalInstrument flautaTravesera = new WindInstrument("flauta travesera", 340.0);
+
+        System.err.println(guitarra.tocar());
+        System.err.println(bateria.tocar());
+        System.err.println(flauta.tocar());
+        System.err.println(flautaTravesera.tocar());
     }
 }
